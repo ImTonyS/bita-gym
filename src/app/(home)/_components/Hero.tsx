9db03f2332d6decec21e701efccfea5ae82f51fb
ImTonyS <tony.villaverde@hotmail.com>
2024-06-05 18:23:18 -0600

    Refactor SVG attributes for consistency and adherence to coding standards

--- src/app/(home)/_components/Hero.tsx
+++ src/app/(home)/_components/Hero.tsx
@@ -29,13 +29,13 @@ export default function Hero() {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="gray"
               className="size-6 animate-bounce"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="m4.5 4.5 15 15m0 0V8.25m0 11.25H8.25"
               />
             </svg>
